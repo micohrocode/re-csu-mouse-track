6e3c73d0f24e6ecb59fb020cab685d54836acba2
saraaly01 <aly.sara01@gmail.com>
2022-07-18 16:32:23 -0400

    Big changes, fixed rectangle flashing, added timer on testing screen

--- version2.py
+++ version2.py
@@ -91,6 +91,9 @@ def cursor_collision(canvas,coords,active,target):
         return True
 
 def main(sval1,sval2,my_w,name, inch, amp, targetWidth ,fileName, cursorVisible, interT, numT):
+    outR = 0
+    outL = 0
+    # variables above see if rectangles r out
     workbook = xlsxwriter.Workbook(fileName)
     outSheet = workbook.add_worksheet()
     my_w_child=Toplevel(my_w) # Child window
@@ -167,7 +170,6 @@ def main(sval1,sval2,my_w,name, inch, amp, targetWidth ,fileName, cursorVisible,
     # set movement velocity threshold, camera fps
     pixel_vel_thresh = math.ceil(math.ceil((inch/25.4) * 30) / 60)
     print('Vel pixel thresh: '+str(pixel_vel_thresh))
-   
     oldTimeCheck = 0
     time.sleep(5)
     cell = 1
@@ -182,6 +184,9 @@ def main(sval1,sval2,my_w,name, inch, amp, targetWidth ,fileName, cursorVisible,
         frame = cv2.flip(frame, -1)
    
     tracker.init(frame,bbox)
+     # find center point
+    window_center_x = window_width / 2
+    window_center_y = window_height / 2
    
     while True:
         if numTrials == numT:
@@ -204,169 +209,181 @@ def main(sval1,sval2,my_w,name, inch, amp, targetWidth ,fileName, cursorVisible,
         frame = cv2.flip(frame, -1)
    
         ok,bbox=tracker.update(frame)
-        if ok:
-            # cursor location
-            (x,y,w,h)=[int(v) for v in bbox]
-            cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2,1)
-            x2 = x + int(w/2)
-            cv2.circle(frame,(x2,y),4,(0,0,255),-1)
-            
-            y_hold = y
-            
-            x2 = int(x2*(inch/(pixelToMM*25.4)))
-            y = int(y*(inch/(pixelToMM*25.4)))
-           
-            # clear canvas for updating
-            myCanvas.delete(ALL)
-           
-            # find center point
-            window_center_x = window_width / 2
-            window_center_y = window_height / 2
-           
-            # rectangle area drawing
+        
+        # cursor location
+        (x,y,w,h)=[int(v) for v in bbox]
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2,1)
+        x2 = x + int(w/2)
+        cv2.circle(frame,(x2,y),4,(0,0,255),-1)
+        
+        y_hold = y
+        
+        x2 = int(x2*(inch/(pixelToMM*25.4)))
+        y = int(y*(inch/(pixelToMM*25.4)))
+        
+        # clear canvas for updating
+        myCanvas.delete(ALL)
+        
+        
+        
+        # rectangle area drawing
+        if outR == 1:
+            rectangleR = draw_rectangle(myCanvas, window_center_x, window_center_y, window_width, window_height,.4, 'right', inch, amp, targetWidth, 'normal')
+        elif outL == 1:
+            rectangleL = draw_rectangle(myCanvas, window_center_x, window_center_y, window_width, window_height, .4,  'left', inch, amp, targetWidth,'normal')
+        else:
             rectangleR = draw_rectangle(myCanvas, window_center_x, window_center_y, window_width, window_height,.4, 'right', inch, amp, targetWidth,'hidden')
             rectangleL = draw_rectangle(myCanvas, window_center_x, window_center_y, window_width, window_height, .4,  'left', inch, amp, targetWidth,'hidden')
-           
-            # cursor updating/drawing
-            r = 5
-            x0 = x2 - r
-            y0 = y - r
-            x1 = x2 + r
-            y1 = y + r
-            
-           
-            # center start position
-            start_center_move = myCanvas.create_rectangle(int(window_center_x - 25),
-                                 int(window_center_y - 25),
-                                 int(window_center_x + 25),
-                                 int(window_center_y + 25),
-                                 outline="black",
-                                 fill="red")
-           
-            cursorOval = myCanvas.create_oval(x0, y0, x1, y1)
-           
-            start_check_move = cursor_collision(myCanvas,myCanvas.coords(start_center_move),2,start_center_move)
-           
-            my_w_child.update()
-           
-            if not start_check_move:
-                start_counter = 0
-                oldTimeCheck = 1
-                oldtime = time.time()
-                
+
+        # cursor updating/drawing
+        r = 5
+        x0 = x2 - r
+        y0 = y - r
+        x1 = x2 + r
+        y1 = y + r
+        
+        
+        # center start position
+        start_center_move = myCanvas.create_rectangle(int(window_center_x - 25),
+                                int(window_center_y - 25),
+                                int(window_center_x + 25),
+                                int(window_center_y + 25),
+                                outline="black",
+                                fill="red")
+        
+        cursorOval = myCanvas.create_oval(x0, y0, x1, y1)
+        if outR == 0 and outL == 0:
+            if start_counter == 0:
+                myCanvas.create_text(300, 50, text="Go to Center", fill="black", font=('Helvetica 30 bold'))
             else:
-                if oldTimeCheck == 0:
-                    oldtime = time.time()
-                    oldTimeCheck = 1
+                countdown = "Stay Still for " + str(start_counter) + " Seconds"
+                myCanvas.create_text(300, 50, text=countdown, fill="black", font=('Helvetica 30 bold'))
+
+
+        start_check_move = cursor_collision(myCanvas,myCanvas.coords(start_center_move),2,start_center_move)
+        
+        my_w_child.update()
+        
+        if not start_check_move:
+            start_counter = 0
+            oldtime = time.time()
+            oldTimeCheck = 1
+        else:
+            if oldTimeCheck == 0:
+                oldtime = time.time()
+                oldTimeCheck = 1
+            else:
                 elapsed = time.time() - oldtime
                 start_counter = interT - int(elapsed)
                 if elapsed >= interT:
-                    print(elapsed)
+                   #print(elapsed)
                     has_been_to_start = True
                     start_counter = 0
+                    oldTimeCheck = 0
                     # choose_rect = random.uniform(0, 1)
-                         
-            # # after they are in the start for long enough check if they leave the start to start move?
-            in_center  = cursor_collision(myCanvas,myCanvas.coords(start_center_move),2,start_center_move)
-             
-            if has_been_to_start:
-              
-                if cursorVisible == 0:
-                    myCanvas.itemconfigure(cursorOval, state = 'hidden')
-                if choose_rect >= .5:
-                    myCanvas.itemconfigure(rectangleR, state='normal')
-                    
-                    my_w_child.update()
-                elif choose_rect < .5:
-                    myCanvas.itemconfigure(rectangleL, state='normal')
-                    
-                    my_w_child.update()
-                
-                # movement checks/data
-                if not prev:
-                    # set orginal check point for movement
-                    prev = (x2,y)
-                    status = "still"
-                elif not in_center:
-                    if prev[0] > x2+pixel_vel_thresh or prev[0] <x2-pixel_vel_thresh and not in_center:
-                        # if moving in x direction
-                        frame = cv2.putText(frame, 'Moving', (x,y_hold), cv2.FONT_HERSHEY_TRIPLEX,
-                                1, (0,255,0), 2)
-                        # get update point of movement and time since start
-                        if start_time:
-                            move_portions.append([x2,y,(datetime.now() - start_time)])
-                        prev = (x2,y)
-                       
-                        # if it was still start a movement
-                        if status == "still":
-                            start  = (x2,y)
-                            start_time = datetime.now()
-                       
-                        status = "moving"
-                    elif prev[1] > y+pixel_vel_thresh or prev[1] <y-pixel_vel_thresh and not in_center:
-                        # if moving in the y direction
-                        frame = cv2.putText(frame, 'Moving', (x,y_hold), cv2.FONT_HERSHEY_TRIPLEX,
-                                1, (0,255,0), 2)
-                        # get update point of movement and time since start
-                        if start_time:
-                            move_portions.append([x2,y,(datetime.now() - start_time)])
-                        prev = (x2,y)
-                       
-                        # if it was still start a movement
-                        if status == "still":
-                            start  = (x2,y)
-                            start_time = datetime.now()
-                       
-                        status = "moving"
-                    elif (prev[0] < x2+pixel_vel_thresh or prev[0] >x2-pixel_vel_thresh) and (prev[1] < y+pixel_vel_thresh or prev[1] >y-pixel_vel_thresh):
-                        # if not moving enough to count as a movement, ie stopped or slowing down
-                        frame = cv2.putText(frame, 'Still', (x,y_hold), cv2.FONT_HERSHEY_TRIPLEX,
-                                1, (0,255,0), 2)
-                       
-                        # if it was in a movement, stop the movement and log the information
-                        if status == "moving":
-                            # stopping point to target?
-                            hit_or_miss = 'miss'
-                            target_center = None
-                            
-                            x1o, y1o, x2o, y2o = myCanvas.coords(cursorOval)
-                            if choose_rect >= .5:
-                                x1r, y1r, x2r, y2r = myCanvas.coords(rectangleR)
-                                target_center = (((x2r+x1r)/2),((y1r+y2r)/2))
-                                if x1o >= x1r and y1o >= y1r and x2o <= x2r and y2o <= y2r:
-                                    myCanvas.itemconfig(rectangleR, fill='green')
-                                    hit_or_miss = 'hit'
-                            else:
-                                x1r, y1r, x2r, y2r = myCanvas.coords(rectangleL)
-                                target_center = (((x2r+x1r)/2),((y1r+y2r)/2))
-                                if x1o >= x1r and y1o >= y1r and x2o <= x2r and y2o <= y2r:
-                                    myCanvas.itemconfig(rectangleL, fill='green')
-                                    hit_or_miss = 'hit'
-                                          
-                            my_w_child.update()
-                            end = (x2,y)
-                            end_time = datetime.now()
-                            movements.append([start,end,math.dist(start, end),(end_time - start_time),move_portions,start_time, math.dist(start, end)/pixelToMM,hit_or_miss,target_center,math.dist(end, target_center)])
-                            move_portions = []
-                            start_time = None
-                            # start new test at the end of each move
-                            has_been_to_start = False
-                            status = "still"
-                            choose_rect = random.uniform(0, 1)
-                            numTrials = numTrials + 1
-                            continue
-                       
-                        status = "still"
-                # outSheet.write(cell, 0, x2)
-                # outSheet.write(cell, 1, y)
-                # cell = cell + 1
+                        
+        # # after they are in the start for long enough check if they leave the start to start move?
+        in_center  = cursor_collision(myCanvas,myCanvas.coords(start_center_move),2,start_center_move)
+            
+        if has_been_to_start:
+            
+            if cursorVisible == 0:
+                myCanvas.itemconfigure(cursorOval, state = 'hidden')
+
+            if choose_rect >= .5:
+                myCanvas.itemconfigure(rectangleR, state='normal')
+                outR = 1
             else:
-                cv2.putText(frame,'Place in center: ' + str(start_counter),(100,100),cv2.FONT_HERSHEY_SIMPLEX,1,(0,0,255),2)
-            cv2.imshow('Tracking',frame)
-            cv2.setWindowProperty("Tracking", cv2.WND_PROP_TOPMOST, 1)
-            if cv2.waitKey(20) & 0xFF==ord('p'):
-               break
-            counter = counter + 1
+                myCanvas.itemconfigure(rectangleL, state='normal')
+                outL = 1
+            my_w_child.update()
+            
+            # movement checks/data
+            if not prev:
+                # set orginal check point for movement
+                prev = (x2,y)
+                status = "still"
+            elif not in_center:
+                if prev[0] > x2+pixel_vel_thresh or prev[0] <x2-pixel_vel_thresh and not in_center:
+                    # if moving in x direction
+                    frame = cv2.putText(frame, 'Moving', (x,y_hold), cv2.FONT_HERSHEY_TRIPLEX,
+                            1, (0,255,0), 2)
+                    # get update point of movement and time since start
+                    if start_time:
+                        move_portions.append([x2,y,(datetime.now() - start_time)])
+                    prev = (x2,y)
+                    
+                    # if it was still start a movement
+                    if status == "still":
+                        start  = (x2,y)
+                        start_time = datetime.now()
+                    
+                    status = "moving"
+                elif prev[1] > y+pixel_vel_thresh or prev[1] <y-pixel_vel_thresh and not in_center:
+                    # if moving in the y direction
+                    frame = cv2.putText(frame, 'Moving', (x,y_hold), cv2.FONT_HERSHEY_TRIPLEX,
+                            1, (0,255,0), 2)
+                    # get update point of movement and time since start
+                    if start_time:
+                        move_portions.append([x2,y,(datetime.now() - start_time)])
+                    prev = (x2,y)
+                    
+                    # if it was still start a movement
+                    if status == "still":
+                        start  = (x2,y)
+                        start_time = datetime.now()
+                    
+                    status = "moving"
+                elif (prev[0] < x2+pixel_vel_thresh or prev[0] >x2-pixel_vel_thresh) and (prev[1] < y+pixel_vel_thresh or prev[1] >y-pixel_vel_thresh):
+                    # if not moving enough to count as a movement, ie stopped or slowing down
+                    frame = cv2.putText(frame, 'Still', (x,y_hold), cv2.FONT_HERSHEY_TRIPLEX,
+                            1, (0,255,0), 2)
+                    
+                    # if it was in a movement, stop the movement and log the information
+                    if status == "moving":
+                        # stopping point to target?
+                        hit_or_miss = 'miss'
+                        target_center = None
+                        
+                        x1o, y1o, x2o, y2o = myCanvas.coords(cursorOval)
+                        if choose_rect >= .5:
+                            x1r, y1r, x2r, y2r = myCanvas.coords(rectangleR)
+                            target_center = (((x2r+x1r)/2),((y1r+y2r)/2))
+                            if x1o >= x1r and y1o >= y1r and x2o <= x2r and y2o <= y2r:
+                                myCanvas.itemconfig(rectangleR, fill='green')
+                                hit_or_miss = 'hit'
+                                my_w_child.update()
+                        else:
+                            x1r, y1r, x2r, y2r = myCanvas.coords(rectangleL)
+                            target_center = (((x2r+x1r)/2),((y1r+y2r)/2))
+                            if x1o >= x1r and y1o >= y1r and x2o <= x2r and y2o <= y2r:
+                                myCanvas.itemconfig(rectangleL, fill='green')
+                                hit_or_miss = 'hit'
+                                my_w_child.update()
+                        
+                        end = (x2,y)
+                        end_time = datetime.now()
+                        movements.append([start,end,math.dist(start, end),(end_time - start_time),move_portions,start_time, math.dist(start, end)/pixelToMM,hit_or_miss,target_center,math.dist(end, target_center)])
+                        move_portions = []
+                        start_time = None
+                        # start new test at the end of each move
+                        has_been_to_start = False
+                        outR = 0
+                        outL = 0
+                        status = "still"
+                        choose_rect = random.uniform(0, 1)
+                        numTrials = numTrials + 1
+                        continue
+                    
+                    status = "still"
+
+        else:
+            cv2.putText(frame,'Place in center: ' + str(start_counter),(100,100),cv2.FONT_HERSHEY_SIMPLEX,1,(0,0,255),2)
+        cv2.imshow('Tracking',frame)
+        cv2.setWindowProperty("Tracking", cv2.WND_PROP_TOPMOST, 1)
+        if cv2.waitKey(20) & 0xFF==ord('p'):
+            break
+        counter = counter + 1
    
     for x in range(len(movements)):
         # start point to end point line
